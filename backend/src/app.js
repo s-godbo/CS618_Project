4b00119d34596f188145f0972c8509cfc7f1c7e1
s-godbo <samantha.godbold@bsu.edu>
2025-09-11 16:09:38 +0000

    chore: built api routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import { postsRoutes } from './routes/posts.js'
+import bodyParser from 'body-parser'
 
 const app = express()
+app.use(bodyParser.json())
+
+postsRoutes(app)
+
 app.get('/', (req, res) => {
   res.send('Hello from Express Nodemon!')
 })
