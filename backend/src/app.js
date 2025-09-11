072049b1e1b20ad5f730a745ef83ad9a15492630
s-godbo <samantha.godbold@bsu.edu>
2025-09-11 16:45:39 +0000

    chore: enabled cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { postsRoutes } from './routes/posts.js'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 
 postsRoutes(app)
 
