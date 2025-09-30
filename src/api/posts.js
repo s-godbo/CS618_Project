0b6bcc2f477f3bc8b0b708d0df0f52d7db9ba6a2
s-godbo <samantha.godbold@bsu.edu>
2025-09-30 19:12:28 +0000

    fix: fixed spacing issue in posts.js file

--- src/api/posts.js
+++ src/api/posts.js
@@ -7,17 +7,13 @@ export const getPosts = async (queryParams) => {
 }
 
 export const createPost = async (token, post) => {
-  const res = await fetch(
-    `${import.meta.env.VITE_BACKEND_URL}/
-        posts`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(post),
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/posts`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
     },
-  )
+    body: JSON.stringify(post),
+  })
   return await res.json()
 }
