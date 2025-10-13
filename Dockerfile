36176812d0db7722a8d713bc2b3bc6ff7d4a65aa
s-godbo <samantha.godbold@bsu.edu>
2025-10-13 16:22:57 +0000

    chore: ssr, seo, metatags, sitemaps, and updates to docker files

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM node:20 AS build
-ARG VITE_BACKEND_URL=https://improved-space-robot-x5v5rgw546g52pq4j-3001.app.github.dev/api/v1
+ARG VITE_BACKEND_URL=https://supreme-space-waddle-77vgp4r7wwfpq7x-3001.app.github.dev/api/v1
 WORKDIR /build
 COPY package.json .
 COPY package-lock.json .
@@ -7,6 +7,8 @@ RUN npm install
 COPY . .
 RUN npm run build
 
-FROM nginx AS final
-WORKDIR /usr/share/nginx/html
-COPY --from=build /build/dist .
+EXPOSE 3000
+
+# Start the SSR server
+CMD ["npm", "start"]
+
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,8 +2,8 @@ import { PostList } from '../components/PostList.jsx'
 import { CreatePost } from '../components/CreatePost.jsx'
 import { PostFilter } from '../components/PostFilter.jsx'
 import { PostSorting } from '../components/PostSorting.jsx'
-
 import { Header } from '../components/Header.jsx'
+import { Helmet } from 'react-helmet-async'
 
 import { useState } from 'react'
 
@@ -24,6 +24,13 @@ export function Blog() {
 
   return (
     <div style={{ padding: 8 }}>
+      <Helmet>
+        <title>Full-Stack React Blog</title>
+        <meta
+          name='description'
+          content='A blog full of articles about full-stack React development.'
+        />
+      </Helmet>
       <Header />
       <br />
       <hr />
