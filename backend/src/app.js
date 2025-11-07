7e1c81e4266933ae7f1a787fa183a7cf828e1442
s-godbo <samantha.godbold@bsu.edu>
2025-11-07 17:05:08 +0000

    chore: setup socketio

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,8 @@ import { postsRoutes } from './routes/posts.js'
 import { userRoutes } from './routes/users.js'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
 
 const app = express()
 app.use(bodyParser.json())
@@ -11,8 +13,22 @@ app.use(cors())
 postsRoutes(app)
 userRoutes(app)
 
+const server = createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+})
+io.on('connection', (socket) => {
+  console.log('user connected:', socket.id)
+  socket.on('disconnect', () => {
+    console.log('user disconnected:', socket.id)
+  })
+})
+
 app.get('/', (req, res) => {
   res.send('Hello from Express Nodemon!')
 })
 
-export { app }
+//export { app }
+export { server as app }
